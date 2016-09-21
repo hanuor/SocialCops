4293beb10d5d3b616acbd50e87fe64e8219e292d
Shantanu Johri <shanjohri@gmail.com>
2016-09-21 06:18:05 +0530

    video functionality added

--- app/src/main/java/com/hanuorsocialcops/socialcops/CameraUtils/CameraFragment.java
+++ app/src/main/java/com/hanuorsocialcops/socialcops/CameraUtils/CameraFragment.java
@@ -4,12 +4,13 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.PixelFormat;
 import android.hardware.Camera;
+import android.media.CamcorderProfile;
+import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.SurfaceHolder;
@@ -27,6 +28,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Random;
 
+import static android.view.View.GONE;
+
 
 /**
  * Created by Shantanu Johri on 9/20/2016.
@@ -41,6 +44,8 @@ public class CameraFragment extends Fragment implements SurfaceHolder.Callback {
     Camera.PictureCallback rawCallback;
     Camera.ShutterCallback shutterCallback;
     Camera.PictureCallback jpegCallback;
+    MediaRecorder mediaRecorder;
+    boolean recording;
 
     String stringPath = "/sdcard/socialcops.jpg";
 
@@ -49,15 +54,18 @@ public class CameraFragment extends Fragment implements SurfaceHolder.Callback {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v=inflater.inflate(R.layout.frag1_fragemet,container,false);
         final FloatingActionButton buttonStartCameraPreview = (FloatingActionButton) v.findViewById(R.id.startcamerapreview);
-        FloatingActionButton buttonStopCameraPreview = (FloatingActionButton) v.findViewById(R.id.stopcamerapreview);
+        final FloatingActionButton videoRecorder = (FloatingActionButton) v.findViewById(R.id.videoRecord);
         final FloatingActionButton captureImage = (FloatingActionButton) v.findViewById(R.id.captureImage);
+        final FloatingActionButton captureVideo = (FloatingActionButton) v.findViewById(R.id.captureVideo);
         getActivity().getWindow().setFormat(PixelFormat.UNKNOWN);
         surfaceView = (SurfaceView) v.findViewById(R.id.surfaceview);
+        recording = false;
+        mediaRecorder = new MediaRecorder();
+        mediaRecoderSettings();
        // frameLayout = (FrameLayout) v.findViewById(R.id.defView);
         surfaceHolder = surfaceView.getHolder();
         surfaceHolder.addCallback(this);
         surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-
 
         rawCallback = new Camera.PictureCallback() {
             public void onPictureTaken(byte[] data, Camera camera) {
@@ -102,8 +110,9 @@ public class CameraFragment extends Fragment implements SurfaceHolder.Callback {
             @Override
             public void onClick(View v) {
                 // TODO Auto-generated method stub
-                buttonStartCameraPreview.setVisibility(View.GONE);
+                buttonStartCameraPreview.setVisibility(GONE);
                 captureImage.setVisibility(View.VISIBLE);
+                videoRecorder.setVisibility(GONE);
                 if(!previewing){
                     camera = Camera.open();
                     if (camera != null){
@@ -119,6 +128,35 @@ public class CameraFragment extends Fragment implements SurfaceHolder.Callback {
                     }
                 }
             }});
+        videoRecorder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonStartCameraPreview.setVisibility(GONE);
+                captureVideo.setVisibility(View.VISIBLE);
+                captureImage.setVisibility(GONE);
+             videoRecorder.setVisibility(GONE);
+
+            }
+        });
+        captureVideo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(recording){
+                    videoRecorder.setBackgroundDrawable(getActivity().getResources().getDrawable(R.drawable.pause));
+                    mediaRecorder.stop();
+                    mediaRecorder.release();
+                    Log.d("trans","stop");
+
+
+                }else{
+                    videoRecorder.setBackgroundDrawable(getActivity().getResources().getDrawable(R.drawable.play));
+                    mediaRecorder.start();
+                    recording = true;
+                    Log.d("trans","start");
+
+                }
+            }
+        });
         captureImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -132,7 +170,7 @@ public class CameraFragment extends Fragment implements SurfaceHolder.Callback {
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
-
+        prepareMediaRecorder();
     }
 
     @Override
@@ -162,6 +200,34 @@ public class CameraFragment extends Fragment implements SurfaceHolder.Callback {
             out.close();
 
         } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    private void mediaRecoderSettings(){
+        Random generator = new Random();
+        int n = 10000;
+        String root = Environment.getExternalStorageDirectory().toString()+"/socialCopsDemo/";
+        n = generator.nextInt(n);
+        String fname = "Vid-"+ n;
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.DEFAULT);
+        mediaRecorder.setVideoSource(MediaRecorder.VideoSource.DEFAULT);
+        CamcorderProfile camcorderProfile_HQ = CamcorderProfile.get(CamcorderProfile.QUALITY_HIGH);
+        mediaRecorder.setProfile(camcorderProfile_HQ);
+        mediaRecorder.setOrientationHint(90);
+        mediaRecorder.setOutputFile(root+fname+".mp4" );
+        mediaRecorder.setMaxDuration(60000); // Set max duration 60 sec.
+        mediaRecorder.setMaxFileSize(5000000); // Set max file size 5M
+    }
+
+    private void prepareMediaRecorder(){
+        mediaRecorder.setPreviewDisplay(surfaceHolder.getSurface());
+        try {
+            mediaRecorder.prepare();
+        } catch (IllegalStateException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
